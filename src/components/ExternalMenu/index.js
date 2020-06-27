0e215c73ce8fdaa16dea45ceeecd0e710443ef30
Jeniffer Carvalho <jeniffer.carvalho@cbyk.com.br>
2020-06-26 21:08:48 -0300

    created ExternalMenu component and style

---
+++ src/components/ExternalMenu/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.scss'
+
+const ExternalMenu = () => {
+  return (
+    <nav className="nav-top">
+      <li><a href="/" className="nav-top__link">Home</a></li>
+      <li><a href="/" className="nav-top__link">Join orkut</a></li>
+      <li><a href="/" className="nav-top__link">Media</a></li>
+      <li><a href="/" className="nav-top__link">Help</a></li>
+    </nav>
+  )
+}
+
+export default ExternalMenu;
