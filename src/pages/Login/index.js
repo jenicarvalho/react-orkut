1f932039298767419126fa06a015bd68dc13058d
Leandro Nunes <nunes.pessoal@gmail.com>
2020-07-03 20:55:36 -0300

    Added BannerIntro and LoginForm components

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,8 +1,19 @@
 import React from 'react'
 import ExternalMenu from '../../components/ExternalMenu'
+import BannerIntro from '../../components/BannerIntro'
+import LoginForm from '../../components/LoginForm'
+import './style.scss'
 
 function Login() {
-  return <ExternalMenu />
+  return (
+  <>
+  <ExternalMenu />
+  <div class="container-intro">
+    <BannerIntro />
+    <LoginForm />
+  </div>
+  </>
+  )
 }
 
 export default Login
