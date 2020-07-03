5e94d390db10b5b6f73950a3c07b5d037ced596d
Leandro Nunes <nunes.pessoal@gmail.com>
2020-07-03 20:53:54 -0300

    Created BannerIntro component

---
+++ src/components/BannerIntro/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.scss'
+import Logo from '../../assets/logo.gif'
+
+const BannerIntro = () => {
+  return (
+    <img src={Logo} />
+  )
+}
+
+export default BannerIntro;
