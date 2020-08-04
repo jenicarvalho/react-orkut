ca10f330b4cfd0c49a6bafe406b8af180390025d
Leandro Nunes <nunes.pessoal@gmail.com>
2020-08-04 08:00:08 -0300

    BannerIntro update

--- src/components/BannerIntro/index.js
+++ src/components/BannerIntro/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './style.scss'
 import Logo from '../../assets/logo.gif'
+import WDYK from '../../assets/i_wdyk.jpg'
 
 const BannerIntro = () => {
   return (
-    <img src={Logo} />
+    <div id="text-intro">
+      <p><img src={Logo} alt="Orkut" /> is an online community that connects people through a network of trusted friends.</p>
+      <p>We are committed to providing an online meeting place where people can socialize, make new acquaintances and find others who share their interests.</p>
+      <img src={WDYK} alt="Who do you know?" id="wdyk" />
+    </div>
   )
 }
 
