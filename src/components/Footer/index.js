fb5966d2c8faa1d63f51f71b6f7ebc83c3fd314e
Leandro Nunes <nunes.pessoal@gmail.com>
2020-08-04 08:00:35 -0300

    Footer creation

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.scss'
+
+const Footer = () => {
+  return (
+    <div id="footer">
+      <div id="disclaimer">in affiliation with Google</div>
+      <div id="links">About orkut | Privacy | Terms  </div>
+    </div>
+  )
+}
+
+export default Footer;
